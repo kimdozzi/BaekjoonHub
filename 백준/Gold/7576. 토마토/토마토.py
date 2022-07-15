881ab241986c091c71252cdf0e3a4d3e3a8f0991
DoHyung Kim <neo5188@gmail.com>
2022-07-15 13:05:33 +0900

    [Gold V] Title: 토마토, Time: 2320 ms, Memory: 97988 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+dx = [-1,1,0,0]
+dy = [0,0,-1,1]
+m,n = map(int,input().split())
+graph = [list(map(int,input().split())) for _ in range(n)]
+queue = deque([])
+res = 0
+
+for i in range(n) :
+    for j in range(m) :
+        if graph[i][j] == 1:
+            queue.append((i,j))
+def bfs():
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4) :
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0 :
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append((nx,ny))
+bfs()
+for tomatoes in graph :
+    for tomato in tomatoes :
+        if tomato == 0 :
+            print(-1)
+            exit(0)
+    res = max(res,max(tomatoes))
+print(res-1)
