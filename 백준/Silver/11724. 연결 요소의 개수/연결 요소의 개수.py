e643b977c7e3fc30b8cb890b8b1fef6849f44bac
DoHyung Kim <neo5188@gmail.com>
2022-07-20 12:46:52 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 780 ms, Memory: 64564 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -0,0 +1,47 @@
+# BFS와 DFS
+import sys
+from collections import deque
+
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+    # stack = [start]  DFS
+    visited[start] = True
+    while queue:
+        v = queue.popleft()
+        #print(v, end=' ')
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+
+def dfs(graph, start, visited):
+    visited[start] = True
+    #print(start, end=' ')
+    for i in graph[start]:
+        if not visited[i]:
+            dfs(graph, i, visited)
+
+
+input = sys.stdin.readline
+n, m = map(int, input().split())
+visited = [False for _ in range(n+1)]
+graph = [[] for _ in range(n+1)]
+ans = 0
+for _ in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+'''
+for i in range(1, n+1):
+    graph[i].sort()
+'''
+
+for i in range(1,n+1) :
+    if visited[i] == False :
+        bfs(graph, i, visited)
+        ans += 1
+
+#print(*graph, sep='\n')
+print(ans)
